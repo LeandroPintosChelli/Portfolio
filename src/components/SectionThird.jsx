f92078ae8a3442e206223532cd949f5cd49a8817
Leandro Pintos <86613539+LeandroPintosChelli@users.noreply.github.com>
2023-07-22 14:44:04 -0300

    Fix responsive style

--- src/components/SectionThird.jsx
+++ src/components/SectionThird.jsx
@@ -3,30 +3,36 @@ const SectionThird = () => {
     <div className="flex justify-center px-5 py-7 bg-white">
       <div className="w-full px-5 py-7">
         <div className="px-5 py-7">
-          <h1 className="font-serif text-xl text-center font-semibold">Not convinced? look at what I have built.</h1>
+          <h1 className="font-serif text-xl text-center font-semibold">
+            Not convinced? look at what I have built.
+          </h1>
         </div>
-        <div className="flex justify-center">
-          <div className="w-360px m-5 w-60 bg-white border-solid border-2 rounded-md shadow-md px-5 py-7">
+        <div className="flex justify-center w-full">
+          <div className="m-5 w-60 bg-white border-solid border-2 rounded-md shadow-md px-5 py-7">
             <h3 className="mb-2 font-bold">Citas React</h3>
             <p className="mb-3">
-              Citas React is an App where you can manage your patients in a veterinary
+              Citas React is an App where you can manage your patients in a
+              veterinary
             </p>
-            <a className="gold" href="https://jovial-lollipop-c8219a.netlify.app">
+            <a
+              className="gold"
+              href="https://jovial-lollipop-c8219a.netlify.app"
+            >
               Click here!
             </a>
           </div>
-          <div className="w-360px w-60 m-5 bg-white border-solid border-2 rounded-md shadow-md px-5 py-7">
-            <h3 className="mb-2 font-bold">In Production</h3>
-            <p>...</p>
+          <div className=" w-60 m-5 bg-white border-solid border-2 rounded-md shadow-md px-5 py-7">
+            <h3 className="mb-2 font-bold">InventoryApp: In Production</h3>
+            <p className="mb-2 font-bold">...</p>
             <a className="gold" href="#">
-              Click here!
+              {/* Click here! */}
             </a>
           </div>
-          <div className="w-360px w-60 m-5 bg-white border-solid border-2 rounded-md shadow-md px-5 py-7">
+          <div className=" w-60 m-5 bg-white border-solid border-2 rounded-md shadow-md px-5 py-7">
             <h3 className="mb-2 font-bold">In Production</h3>
-            <p>...</p>
+            <p className="mb-2 font-bold">...</p>
             <a className="gold" href="#">
-              Click here!
+              {/* Click here! */}
             </a>
           </div>
         </div>
