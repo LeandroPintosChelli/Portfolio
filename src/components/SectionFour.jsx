40494d28199f8f90f729c175c7cbc9805c0eeb86
Leandro Pintos <86613539+LeandroPintosChelli@users.noreply.github.com>
2023-07-22 19:07:22 -0300

    Fix responsive of contact section

--- src/components/SectionFour.jsx
+++ src/components/SectionFour.jsx
@@ -1,10 +1,10 @@
 const SectionFour = () => {
   return (
-    <div id="contacto" className="flex justify-center items-center py-7">
+    <div id="contacto" className="flex justify-center items-center">
       <div className="w-full">
-        <div className="flex flex-row py-0 justify-center">
-          <div className="self-center px-5 py-7">
-            <h1 className="font-semibold text-xl mb-5">
+        <div className="flex flex-col py-0 justify-center">
+          <div className="self-center px-5 py-7 justify-center">
+            <h1 className="text-center font-semibold text-xl mb-5">
               Now or never! Let's work together.
             </h1>
             <p>
@@ -12,11 +12,15 @@ const SectionFour = () => {
               long term relationship.
             </p>
           </div>
-          <div className="self-center px-5 py-7 flex-col">
-            <h3 className="text-lg font-serif font-semibold">Email:</h3>
-            <p className="font-serif mb-4"> leandropintos162@gmail.com</p>
-            <h3 className="text-lg font-serif font-semibold">LinkedIn:</h3>
-            <p className="font-serif">Leandro Pintos Chelli</p>
+          <div className="flex px-5 py-7 flex-row justify-center">
+            <div className="px-5">
+              <h3 className="text-lg font-serif font-semibold">Email:</h3>
+              <p className="font-serif mb-4"> leandropintos162@gmail.com</p>
+            </div>
+            <div className="px-5">
+              <h3 className="text-lg font-serif font-semibold">LinkedIn:</h3>
+              <p className="font-serif">Leandro Pintos Chelli</p>
+            </div>
             {/*  <form
           className="contact-form"
           method="post"
